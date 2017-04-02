dba8fb78c4a2fcb0995d71276b26c9f5d7f8ec86
Dihara Wijetunga <shredhead94@gmail.com>
2017-04-02 18:05:31 +0530

    Setup application framework.

---
+++ src/1-hello-vulkan/application.cpp
@@ -0,0 +1,86 @@
+#include "application.h"
+#include "const.h"
+
+bool Application::keys[1024];
+bool Application::mouse[5];
+
+Application::Application()
+{
+
+}
+
+Application::~Application()
+{
+
+}
+
+bool Application::init_internal()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	_window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, nullptr, nullptr);
+
+	glfwSetKeyCallback(_window, key_callback);
+	glfwSetCursorPosCallback(_window, mouse_callback);
+	glfwSetScrollCallback(_window, scroll_callback);
+
+	if (_window)
+		return true;
+	else
+		return false;
+}
+
+void Application::shutdown_internal()
+{
+	glfwDestroyWindow(_window);
+	glfwTerminate();
+}
+
+void Application::update()
+{
+	glfwPollEvents();
+}
+
+void Application::run()
+{
+	if (!init_internal())
+		return;
+
+	if (!init())
+		return;
+
+	while (!glfwWindowShouldClose(_window))
+	{
+		update();
+		render();
+	}
+
+	shutdown();
+	shutdown_internal();
+}
+
+void Application::key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			keys[key] = true;
+		else if (action == GLFW_RELEASE)
+			keys[key] = false;
+	}
+}
+
+void Application::mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+
+}
+
+void Application::scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+
+}
---
+++ src/1-hello-vulkan/application.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+//#if defined(WIN32)
+//#define EXPERIMENT_DECLARE_MAIN(x)								\
+//int WINAPI WinMain(HINSTANCE hinstance,							\
+//									 HINSTANCE hprevinstance,	\
+//								     LPSTR lpcmdline,			\
+//									 int ncmdshow)				\
+//{																\
+//	Application* app = new x();									\
+//	app->run();													\
+//	delete app;													\
+//	return 0;													\
+//}																\
+//
+//#else
+
+#define EXPERIMENT_DECLARE_MAIN(x)								\
+int main()														\
+{																\
+	Application* app = new x();									\
+	app->run();													\
+	delete app;													\
+	return 0;													\
+}																\
+
+//#endif
+
+class Application
+{
+public:
+	Application();
+	virtual ~Application();
+	void run();
+
+private:
+	bool init_internal();
+	void shutdown_internal();
+	void update();
+
+	static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+	static void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+	static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+	virtual bool init() = 0;
+	virtual void render() = 0;
+	virtual void shutdown() = 0;
+
+public:
+	static bool keys[1024];
+	static bool	mouse[5];
+
+private:
+	GLFWwindow* _window;
+};
